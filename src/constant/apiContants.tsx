63f8db35279ca884967ef4c0cadc02684daeee68
Vũ Lê Bảo Long <vulebaolong@gmail.com>
2024-01-01 20:59:09 +0700

    oke

--- src/constant/apiContants.tsx
+++ src/constant/apiContants.tsx
@@ -1,7 +1,7 @@
 // const isProduction = import.meta.env.PROD;
 
-// export const BASE_URL_API = isProduction ? "https://l9-learning.onrender.com/api/v1" : "http://localhost:3001/api/v1";
-export const BASE_URL_API = 'http://localhost:6969/api/v1';
+// export const BASE_URL_API = 'http://localhost:6969/api/v1';
+export const BASE_URL_API = 'https://be-job-long.vercel.app/api/v1';
 export const BASE_URL_SERVER = 'http://localhost:6969';
 export const API_LOGIN = 'auth/login';
 export const API_REFRESH_TOKEN = 'auth/refresh';
